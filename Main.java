37bf804f0e51b1f3d52b761042a3d395f695e019
tkcheon <tlswlsrhd@gmail.com>
2025-01-15 23:07:56 +0900

    10871

--- Main.java
+++ Main.java
@@ -1,9 +1,25 @@
+import java.util.Scanner;
+
 public class Main {
 	public static void main(String[] args) {
         
-        System.out.print("Hello World!");
+        Scanner sc = new Scanner(System.in);
         
-    }
-    
+        int N = sc.nextInt();
+        int X = sc.nextInt();
+        int count = 0;
 
+        int arr[] = new int[N];
+
+
+        for (int i=0; i < N; i++){
+            arr[i] = sc.nextInt();
+            if (arr[i] < X) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+
+        sc.close();
+
+    }
 }
